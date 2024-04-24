6dc4c0dea2170b7c5bf89cfa2cab7e3a28c37171
CosmicSleepWalker <dev.Amadeush@gmail.com>
2024-04-24 03:48:28 -0500

    added comments to script so that I can comeback and refactor in the future to make the code cleaner

--- script.js
+++ script.js
@@ -23,24 +23,41 @@ let secondNum = 0;
 let operator = "";
 let total = 0;
 btn.addEventListener("click", (e) => {
+    // checks to see if the event target is a button
     if(e.target.matches('button')){
+        // retrieves the current bullet value
         let value = e.target.innerHTML;
+        // checks to see if the number is a integer
         if(Number.isInteger(parseInt(value))){
+            // if operator is empty
             if(operator === ""){
+                // append it to first num
                 firstNum = firstNum * 10 + parseInt(value);
+                // update display
                 document.querySelector("#user-input").value = firstNum;
             }else{
+                // otherwise append it to second number
                 secondNum = secondNum * 10 + parseInt(value);
+                // update display
                 document.querySelector("#user-input").value = secondNum;
             }
+        // else if the value is an operator
         }else if(value === "+" || value === "-" || value === "/" || value === "*"){
+            // second number is not empty
             if(operator && secondNum !== ""){
+                // operate
                 total = operate(parseFloat(firstNum), parseFloat(secondNum), operator);
+                // update display
                 document.querySelector("#user-input").value = total;
+                // reassign first num to total
                 firstNum = total;
+                // reassign secondnum to 0
                 secondNum = 0;
+            // otherwise firstnum is not empty
             }else if(firstNum !== ""){
+                // assign operator to the current value
                 operator  = value;
+                // update the display with first number
                 document.querySelector("#user-input").value = firstNum;
             }
         }else if(value === "clear"){
